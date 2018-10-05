f3cf8ea232545e23f626bf5134267b2f0276097d
Adrian Lyall Cochrane <alcinnz@lavabit.com>
2018-10-05 13:36:03 +1300

    Remove autoslugfield dependency.

--- chronos/models.py
+++ chronos/models.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from autoslug import AutoSlugField
 
 # Create your models here.
 class Album(models.Model):
     title = models.CharField(max_length=400, null=False)
     description = models.TextField(null=True, blank=True)
-    href = models.URLField(null=True, blank=True)
-    slug = AutoSlugField(null=False, default="old", unique=True, populate_from="title")
+    href = models.URLField(null=True, blank=True, verbose_name="link")
+    slug = models.SlugField(null=False, default="old", unique=True)
 
     start = models.DateField(null=True, blank=True)
     end = models.DateField(null=True, blank=True)
